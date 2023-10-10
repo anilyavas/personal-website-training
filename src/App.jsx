16081204c2cdfc196928268739790a591b1b8ecf
Anıl <anil@Anl-MacBook-Air.local>
2023-10-10 20:09:37 +0300

    first 3 sections completed

--- src/App.jsx
+++ src/App.jsx
@@ -33,17 +33,65 @@ function App() {
           <h2 className="skills-header">My Skills</h2>
           <h1 className="expertise-header">My Expertise</h1>
         </div>
-        <div className="skills-container">
-          <div>
-            <h1>Web Development</h1>
+
+        <div className="skill-container">
+          <div className="container-part">
+            <h1 className="skillname">Web Development</h1>
             <p>
-              Using technologies such as HTML,CSS and Javascript, I can design
-              and build websites with best user experience.
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+              vel sapien leo. Ut interdum risus mi, vitae pulvinar felis
+              porttitor vel. Duis condimentum tellus vel pellentesque viverra.
+              Phasellus pellentesque auctor.
             </p>
           </div>
-          <div></div>
-          <div></div>
-          <div></div>
+          <div className="container-part">
+            <h1 className="skillname">App Development</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+              vel sapien leo. Ut interdum risus mi, vitae pulvinar felis
+              porttitor vel. Duis condimentum tellus vel pellentesque viverra.
+              Phasellus pellentesque auctor.
+            </p>
+          </div>
+          <div className="container-part">
+            <h1 className="skillname">UI/UX Design</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+              vel sapien leo. Ut interdum risus mi, vitae pulvinar felis
+              porttitor vel. Duis condimentum tellus vel pellentesque viverra.
+              Phasellus pellentesque auctor.
+            </p>
+          </div>
+          <div className="container-part">
+            <h1 className="skillname">Branding/Logo</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+              vel sapien leo. Ut interdum risus mi, vitae pulvinar felis
+              porttitor vel. Duis condimentum tellus vel pellentesque viverra.
+              Phasellus pellentesque auctor.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="about-me">
+        <img className="about-me-img" src={profile} alt="" />
+        <div className="about-me-txt">
+          <h2>About Me</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+            commodo vehicula orci vel vehicula. Nunc ipsum metus, sollicitudin
+            quis tincidunt sit amet, scelerisque vel est. Nunc congue venenatis
+            lorem ac auctor. Fusce vehicula volutpat maximus. Nam ut diam
+            ornare, eleifend lorem id, tempor neque. Donec placerat nisl nec
+            tincidunt porta. Nullam ornare arcu non neque scelerisque, id
+            interdum felis pretium. Suspendisse ut erat felis. Suspendisse
+            potenti. In faucibus metus et sollicitudin vehicula. Vestibulum
+            justo nunc, mollis a ex non, convallis facilisis lorem. Curabitur eu
+            lectus leo. Maecenas efficitur efficitur urna et elementum. Cras
+            volutpat consequat lorem, vitae sollicitudin elit bibendum et. Nulla
+            ut dui diam. Nulla eu augue sit amet elit consequat euismod non
+            feugiat neque.
+          </p>
         </div>
       </div>
     </div>
